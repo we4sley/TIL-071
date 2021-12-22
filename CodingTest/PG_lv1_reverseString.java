6bd393efc5d41226b9f5eb28717beee389013b99
we4sley <galaxy138742@gmail.com>
2021-12-22 23:08:47 +0900

    프로그래머스 레벨1

---
+++ CodingTest/PG_lv1_reverseString.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+import java.util.Collections;
+
+class Solution {
+    public String solution(String s) {
+        String answer = "";
+
+        String[] str = s.split("");
+
+        Arrays.sort(str, Collections.reverseOrder());
+
+        for(int i = 0; i < str.length; i++) {
+            answer += str[i];
+        }
+
+        return answer;
+    }
+}
